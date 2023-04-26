a6877bce8b5e6646d854e3efd7631361a838cb05
meet-javiya0 <meetjaviya29@gmail.com>
2023-04-26 16:34:29 +0530

    Updated Login Page for customer

--- app/src/main/java/com/example/farmconnecta/CustomerLoginPage.kt
+++ app/src/main/java/com/example/farmconnecta/CustomerLoginPage.kt
@@ -1,11 +1,19 @@
 package com.example.farmconnecta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CustomerLoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_login_page)
+
+        val btnSignUP = findViewById<Button>(R.id.btnSignUp)
+        btnSignUP.setOnClickListener {
+            val intent = Intent(this, )
+            startActivity(intent)
+        }
     }
 }
