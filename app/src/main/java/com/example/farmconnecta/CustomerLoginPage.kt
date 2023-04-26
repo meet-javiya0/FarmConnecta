814face12cbbfa93d1ccc74bf4c03d36afa7e5b8
meet-javiya0 <meetjaviya29@gmail.com>
2023-04-26 22:24:44 +0530

    Updated login page

--- app/src/main/java/com/example/farmconnecta/CustomerLoginPage.kt
+++ app/src/main/java/com/example/farmconnecta/CustomerLoginPage.kt
@@ -3,7 +3,6 @@ package com.example.farmconnecta
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -13,19 +12,21 @@ import com.google.firebase.database.FirebaseDatabase
 
 class CustomerLoginPage : AppCompatActivity() {
     private lateinit var databaseReference: DatabaseReference
+
     companion object {
-        const val KEY1 = "com.example.readandusedatafromdatabase.email"
-        const val KEY2 = "com.example.readandusedatafromdatabase.phoneNumber"
+        const val KEY1 = "com.example.farmconnecta.email"
+        const val KEY2 = "com.example.farmconnecta.phoneNumber"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_login_page)
 
-        val etMail = findViewById<TextInputEditText>(R.id.etEmail)
-        val etPassword = findViewById<TextInputEditText>(R.id.etPassword)
+        val etMail = findViewById<TextInputEditText>(R.id.login_email)
+        val etPassword = findViewById<TextInputEditText>(R.id.login_password)
 
-        val btnSignUP = findViewById<Button>(R.id.btnSignUp)
-        btnSignUP.setOnClickListener {
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+        btnSignUp.setOnClickListener {
             val intent = Intent(this, CustomerSignUpPage::class.java)
             startActivity(intent)
         }
@@ -34,55 +35,36 @@ class CustomerLoginPage : AppCompatActivity() {
         btnLogin.setOnClickListener {
             val email = etMail.text.toString()
             val password = etPassword.text.toString()
+
             if (email.isNotEmpty() && password.isNotEmpty()) {
                 readData(email, password)
             } else {
-                Toast.makeText(this, "Please enter your Username!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please enter your email and password!", Toast.LENGTH_SHORT).show()
             }
-            handleSignIn()
         }
     }
 
-    private fun handleSignIn() {
-        val email = findViewById<EditText>(R.id.signup_email).text.toString().trim()
-        val password = findViewById<EditText>(R.id.password_toggle).text.toString().trim()
+    private fun readData(email: String, password: String) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+        databaseReference.orderByChild("email").equalTo(email).get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                for (userSnapshot in dataSnapshot.children) {
+                    val phoneNumber = userSnapshot.child("phoneNumber").value as String
+                    val name = userSnapshot.child("name").value as String
 
-        if (email.isEmpty()) {
-            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show()
-            return
-        }
+                    val intentWelcome = Intent(this, CustomerMainHomePage::class.java)
+                    intentWelcome.putExtra(KEY1, email)
+                    intentWelcome.putExtra(KEY2, phoneNumber)
+                    startActivity(intentWelcome)
 
-        if (password.isEmpty()) {
-            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
-            return
-        }
-    }
-
-    private fun readData(email : String, password : String) {
-        databaseReference = FirebaseDatabase.getInstance().getReference("phoneNumber")
-        databaseReference.child(phoneNumber).get().addOnSuccessListener {
-            // If user exist
-            if (it.exists()) {
-                val email = it.child("email").value
-                val name = it.child("name").value
-                val username = it.child("username").value
-
-                val intentWelcome = Intent(this, CustomerMainHomePage::class.java)
-                intentWelcome.putExtra(KEY1, email.toString())
-                intentWelcome.putExtra(KEY2, name.toString())
-                startActivity(intentWelcome)
+                    Toast.makeText(this, "Welcome $name", Toast.LENGTH_SHORT).show()
+                    return@addOnSuccessListener
+                }
+            } else {
+                Toast.makeText(this, "User does not exist, please register first!", Toast.LENGTH_SHORT).show()
             }
-            // If user not exist
-            else {
-                Toast.makeText(
-                    this,
-                    "User does not exist, Please register first!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }.addOnFailureListener {
-            Toast.makeText(this, "Failed to load!, Please try again later", Toast.LENGTH_SHORT)
-                .show()
+        }.addOnFailureListener { exception ->
+            Toast.makeText(this, "Failed to read data from database. Error: ${exception.message}", Toast.LENGTH_SHORT).show()
         }
     }
 }
