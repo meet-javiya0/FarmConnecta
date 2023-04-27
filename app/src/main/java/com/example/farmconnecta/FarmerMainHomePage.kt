b1b87ec0b9043b2b33a0458701e1649190efe635
meet-javiya0 <meetjaviya29@gmail.com>
2023-04-27 15:32:35 +0530

    Updated farmer main home page

--- app/src/main/java/com/example/farmconnecta/FarmerMainHomePage.kt
+++ app/src/main/java/com/example/farmconnecta/FarmerMainHomePage.kt
@@ -5,8 +5,11 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class FarmerMainHomePage : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_farmer_main_home_page)
@@ -36,8 +39,32 @@ class FarmerMainHomePage : AppCompatActivity() {
                 Toast.makeText(this, "Invalid phone number", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            // Create data class for all the data
+            val orderDetails = FarmerProductData(
+                fNameString,
+                phoneNumberString,
+                productNameString,
+                productWeightString,
+                productLocationString,
+                productPriceString
+            )
+
+            database = FirebaseDatabase.getInstance().getReference("OrderList")
+            database.child(phoneNumberString).setValue(orderDetails).addOnSuccessListener {
+                Toast.makeText(this, "User Registered Successfully", Toast.LENGTH_SHORT).show()
+                fName.setText("")
+                phoneNumber.setText("")
+                productName.setText("")
+                productWeight.setText("")
+                productLocation.setText("")
+                productPrice.setText("")
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
     private fun isValidPhoneNumber(phoneNumber: String): Boolean {
         val phonePattern = "^[+]?[0-9]{10,13}\$"
         return phoneNumber.matches(phonePattern.toRegex())
---
+++ app/src/main/java/com/example/farmconnecta/FarmerProductData.kt
@@ -0,0 +1,10 @@
+package com.example.farmconnecta
+
+data class FarmerProductData(
+    val fName: String,
+    val phoneNumber: String,
+    val productName: String,
+    val productWeight: String,
+    val productLocation: String,
+    val productPrice: String
+)
